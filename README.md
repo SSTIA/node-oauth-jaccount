e4b258204f3056741b6726e2b3cb96adef3d996f
tc-imba <liuyh615@sjtu.edu.cn>
2017-12-31 17:48:34 +0800

    add support for basic jaccount login, logout and API

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
 # node-oauth-jaccount
 
 A simple warpper for SJTU Jaccount OAuth2 login
+
+## Initialization
+
+### Init with `node-config`
+
+In your config file, add a item called `oauth-jaccount`
+with your configs, and simply call this
+
+```
+const oauth_jaccount = require('./oauth-jaccount');
+const jaccount = new oauth_jaccount;
+```
+
+or if you want to use your own item name, use
+
+```
+const oauth_jaccount = require('./oauth-jaccount');
+const jaccount = new oauth_jaccount('yourItemName');
+```
+
+### Init with constructor
+
+```
+const oauth_jaccount = require('./oauth-jaccount');
+const jaccount = new oauth_jaccount({
+    'client_id': 'yourCliendId',
+    'client_secret': 'yourClientSecret'
+});
+```
+
+## Usage
+
+
+
+## Default config
+```
+const defaultConfigs = {
+    client_id: null,
+    client_secret: null,
+    host: 'https://jaccount.sjtu.edu.cn/',
+    authorize_path: 'oauth2/authorize',
+    access_token_path: 'oauth2/token'
+};
+```
+
+You should setup `client_id` and `client_secret` yourselves
--- oauth-jaccount.js
+++ oauth-jaccount.js
@@ -1,30 +1,133 @@
-import config from 'config';
-import oauth from 'oauth';
+const config = require('config');
+const oauth = require('oauth');
+const crypto = require('crypto');
+const querystring = require('querystring');
 
 const PACKAGE_NAME = 'oauth-jaccount';
+const defaultConfigs = {
+    client_id: null,
+    client_secret: null,
+    host: 'https://jaccount.sjtu.edu.cn/',
+    authorize_path: 'oauth2/authorize',
+    access_token_path: 'oauth2/token',
+    logout_path: 'oauth2/logout',
+    api_host: 'https://api.sjtu.edu.cn/',
+    profile_path: 'v1/me/profile'
+};
 
 class OAuthJaccount {
 
     constructor(configs = {}) {
-        const defaultConfigs = {
-            client_id: "value1",
-            client_secret: 22,
-            host: ,
-            authorize_path:
-            access_token_path:
-        };
-        let name = PACKAGE_NAME;
-        if (typeof configs === 'string') {
-            name = configs;
+        this.name = PACKAGE_NAME;
+        if (configs instanceof String) {
+            this.name = configs;
         } else {
             config.util.extendDeep(defaultConfigs, configs);
         }
-        config.util.setModuleDefaults(name, defaultConfigs);
+        config.util.setModuleDefaults(this.name, defaultConfigs);
+        this.examine();
     }
 
+    getConfig(attr) {
+        const key = this.name + '.' + attr;
+        if (config.has(key)) {
+            return config.get(key);
+        }
+        return null;
+    }
 
+    error(message) {
+        console.error('ERROR: oauth-jaccount: ' + message);
+    }
 
+    examine() {
+        this.configMap = {};
+        let flag = true;
+        Object.keys(defaultConfigs).forEach(value => {
+            let configValue = this.getConfig(value);
+            this.configMap[value] = configValue;
+            if (!configValue) {
+                flag = false;
+                this.error(value + ' not found');
+            }
+        });
+        this.oauth2 = new oauth.OAuth2(
+            this.configMap['client_id'],
+            this.configMap['client_secret'],
+            this.configMap['host'],
+            this.configMap['authorize_path'],
+            this.configMap['access_token_path'],
+            null
+        );
+        return flag;
+    }
+
+    getAuthorizeURL(redirect_uri) {
+        const randomState = crypto.randomBytes(32).toString('hex');
+        return this.oauth2.getAuthorizeUrl({
+            redirect_uri: redirect_uri,
+            state: randomState,
+            response_type: 'code'
+        });
+    }
+
+    getLogoutURL(redirect_uri) {
+        const params = {post_logout_redirect_uri: redirect_uri};
+        return this.configMap['host'] + this.configMap['logout_path']
+            + "?" + querystring.stringify(params);
+    }
+
+    getToken(code, redirect_uri) {
+        return new Promise((resolve, reject) => {
+            this.oauth2.getOAuthAccessToken(
+                code, {
+                    grant_type: 'authorization_code',
+                    redirect_uri: redirect_uri
+                }, (e, access_token, refresh_token, results) => {
+                    resolve({
+                        e: e,
+                        access_token: access_token,
+                        refresh_token: refresh_token,
+                        results: results
+                    });
+                }
+            );
+        });
+    }
+
+    refreshToken(refresh_token, redirect_uri) {
+        return new Promise((resolve, reject) => {
+            this.oauth2.getOAuthAccessToken(
+                refresh_token, {
+                    grant_type: 'refresh_token',
+                    redirect_uri: redirect_uri
+                }, (e, access_token, refresh_token, results) => {
+                    resolve({
+                        e: e,
+                        access_token: access_token,
+                        refresh_token: refresh_token,
+                        results: results
+                    });
+                }
+            );
+        });
+    }
+
+    getProfile(access_token) {
+        const url = this.configMap['api_host'] + this.configMap['profile_path'];
+        return new Promise((resolve, reject) => {
+            this.oauth2.get(url, access_token, (error, data) => {
+                if (error) {
+                    if (error.statusCode == 404) error.url = url;
+                    reject(error);
+                }
+                else {
+                    resolve(JSON.parse(data));
+                }
+            });
+        });
+    }
 
 }
 
-export default OAuthJaccount;
+module.exports = OAuthJaccount;
